0121c5178d21d05f3c6af01e8ca81aed41d1f563
saward <mark.saward@episub.com>
2018-03-05 14:06:31 +1100

    added material-ui example

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Button from 'material-ui/Button';
 
 ReactDOM.render(
-	<div>Hello</div>,
+	<div>
+		<Button variant="raised" >
+      Default New Thing
+    </Button>
+	</div>,
   document.getElementById('root'));
